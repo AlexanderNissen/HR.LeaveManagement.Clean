ee54a98870e5dbb3960005b8016f47eaa3c370db
AlexanderNissen <alexandernissen.mail@gmail.com>
2024-06-30 22:15:07 +0200

    Add AutoMapper to proj

---
+++ HR.LeaveManagement.BlazorUI/MappingProfiles/MappingConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace HR.LeaveManagement.BlazorUI.MappingProfiles;
+
+public class MappingConfiguration : Profile
+{
+    public MappingConfiguration()
+    {
+        CreateMap<LeaveTypeDto, LeaveTypeViewModel>().ReverseMap();
+        CreateMap<CreateLeaveTypeCommand, LeaveTypeViewModel>();
+        CreateMap<UpdateLeaveTypeCommand, LeaveTypeViewModel>();
+    }
+}
