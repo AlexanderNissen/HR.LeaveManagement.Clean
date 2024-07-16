ada0fcf1f51ebb0e0bcf1bb04ed62345f78d73db
AlexanderNissen <alexandernissen.mail@gmail.com>
2024-07-16 19:14:44 +0200

    Add AuthService and methods for logging in, registering and generating JWT

---
+++ HR.LeaveManagement.Identity/Services/AuthService.cs
@@ -0,0 +1,112 @@
+﻿using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Identity;
+using HR.LeaveManagement.Application.Models.Identity;
+using HR.LeaveManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace HR.LeaveManagement.Identity.Services;
+
+public class AuthService : IAuthService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly JwtSettings _jwtSettings;
+
+    public AuthService(
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager,
+        JwtSettings jwtSettings)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _jwtSettings = jwtSettings;
+    }
+
+    public async Task<AuthResponse> Login(AuthRequest request)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Email) 
+            ?? throw new NotFoundException($"User with {request.Email} not found.", request.Email);
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+        if (result.Succeeded is false)
+        {
+            throw new BadRequestException($"Credentials for '{request.Email} are invalid");
+        }
+
+        JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+
+        var response = new AuthResponse
+        {
+            Id = user.Id,
+            Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+            Email = user.Email,
+            UserName = user.UserName
+        };
+
+        return response;
+    }
+
+    public async Task<RegistrationResponse> Register(RegistrationRequest request)
+    {
+        var user = new ApplicationUser
+        {
+            Email = request.Email,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Username = request.Username,
+            IsEmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        if (result.Succeeded is false)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var err in result.Errors)
+            {
+                stringBuilder.AppendFormat("- {0}\n", err.Description);
+            }
+
+            throw new BadRequestException($"{stringBuilder}");
+        }
+        await _userManager.AddToRoleAsync(user, "Employer");
+        return new RegistrationResponse() { UserId = user.Id };
+    }
+
+    private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+    {
+        var userClaims = await _userManager.GetClaimsAsync(user);
+        var roles = await _userManager.GetRolesAsync(user);
+
+        var roleClaims = roles.Select(q => new Claim(ClaimTypes.Role, q)).ToList();
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim("uid", user.Id)
+        }
+        .Union(userClaims)
+        .Union(roleClaims);
+
+        var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+
+        var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+        var jwtSecurityToken = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(_jwtSettings.DurationInMinutes),
+            signingCredentials: signingCredentials);
+
+        return jwtSecurityToken;
+    }
+
+}
